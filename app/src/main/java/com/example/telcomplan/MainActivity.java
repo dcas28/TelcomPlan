1c410fe1b598be8541b384f5e47b9a9d48e0b07f
Edrean Casuyon <102269372+dcas28@users.noreply.github.com>
2023-03-05 09:59:50 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/telcomplan/MainActivity.java
+++ app/src/main/java/com/example/telcomplan/MainActivity.java
@@ -46,6 +46,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             case R.id.nav_home:
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
 //                test
+                //edit
                 break;
             case R.id.nav_user:
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new AccountFragment()).commit();
